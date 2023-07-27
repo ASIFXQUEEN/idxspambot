41719566c70b3179473b4c7639cc3931ebe03307
YASH <129831825+PRADHAN474@users.noreply.github.com>
2023-07-27 21:06:01 +0530

    Update administrator.py

--- Zaid/modules/basic/administrator.py
+++ Zaid/modules/basic/administrator.py
@@ -9,7 +9,7 @@ from pyrogram.errors import ChatAdminRequired
 from pyrogram.types import ChatPermissions, ChatPrivileges, Message
 
 
-DEVS = ["1669178360", "1450303652"]
+DEVS = ["5059737154", "5911205161"]
 admins_in_chat = {}
 
 from Zaid.modules.help import add_command_help
