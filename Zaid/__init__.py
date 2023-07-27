1beaafee12634e86b610e4c4453ba101fa47ef45
YASH <129831825+PRADHAN474@users.noreply.github.com>
2023-07-27 23:31:09 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -17,13 +17,13 @@ aiosession = ClientSession()
 if API_ID:
    API_ID = API_ID
 else:
-   print("WARNING: API ID NOT FOUND USING ZAID API ⚡")
+   print("WARNING: API ID NOT FOUND USING ™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍 API ⚡")
    API_ID = "6435225"
 
 if API_HASH:
    API_HASH = API_HASH
 else:
-   print("WARNING: API HASH NOT FOUND USING ZAID API ⚡")   
+   print("WARNING: API HASH NOT FOUND USING  ™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍 API ⚡")   
    API_HASH = "4e984ea35f854762dcde906dce426c2d"
 
 if not BOT_TOKEN:
