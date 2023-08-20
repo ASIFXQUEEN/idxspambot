9f2afd8e679afae2db782a9a0c9e9ca5275091ed
OXYEGN <129831825+PRADHAN474@users.noreply.github.com>
2023-08-20 20:15:22 +0530

    Update start.py

--- Zaid/modules/bot/start.py
+++ Zaid/modules/bot/start.py
@@ -1,10 +1,6 @@
 from Zaid import app, API_ID, API_HASH
-from config import OWNER_ID, ALIVE_PIC
+from config import ALIVE_PIC
 from pyrogram import filters
-import os
-import re
-import asyncio
-import time
 from pyrogram import *
 from pyrogram.types import * 
 
@@ -12,7 +8,7 @@ PHONE_NUMBER_TEXT = (
     "✘ Heya My Master✨🥀!\n\n✘ I'm Your Assistant?\n\n‣ I can help you to host Your Left Clients.\n\n‣ 2.0 \n\n‣ This specially for Buzzy People's(lazy)\n\n‣ Now /clone {send your PyroGram String Session}"
 )
 
-@app.on_message(filters.user(OWNER_ID) & filters.command("start"))
+@app.on_message(filters.command("start"))
 async def hello(client: app, message):
     buttons = [
            [
@@ -25,8 +21,7 @@ async def hello(client: app, message):
     reply_markup = InlineKeyboardMarkup(buttons)
     await client.send_photo(message.chat.id, ALIVE_PIC, caption=PHONE_NUMBER_TEXT, reply_markup=reply_markup)
 
-# © By Itz-Zaid Your motherfucker if uh Don't gives credits.
-@app.on_message(filters.user(OWNER_ID) & filters.command("clone"))
+@app.on_message(filters.command("clone"))
 async def clone(bot: app, msg: Message):
     chat = msg.chat
     text = await msg.reply("Usage:\n\n /clone session")
@@ -34,7 +29,7 @@ async def clone(bot: app, msg: Message):
     phone = msg.command[1]
     try:
         await text.edit("Booting Your Client")
-                   # change this Directry according to ur repo
+        # change this Directory according to your repo
         client = Client(name="Melody", api_id=API_ID, api_hash=API_HASH, session_string=phone, plugins=dict(root="Zaid/modules"))
         await client.start()
         user = await client.get_me()
