7e17882d2e7841891e04e8e9c915a8af4514661f
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-14 00:28:11 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -25,7 +25,7 @@ async def module_help(client: Client, message: Message):
     if len(cmd) > 1:
         help_arg = " ".join(cmd[1:])
     elif not message.reply_to_message and len(cmd) == 1:
-        await message.edit("™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍")
+        await message.edit("™°‌ 𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑")
         try:
             nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
             await asyncio.gather(
@@ -38,13 +38,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍"
+            ac.title = "™°‌ 𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @PRADHAN474 × @BWANDARLOK •",
+                f"```{str(ac)}```\n• @shiva_ansh_op × @mastiwithfriendsx •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @PRADHAN474"
+            this_command += "© @shiva_ansh_op"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -80,12 +80,12 @@ async def module_helper(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "™°‌ 🫧 🇴 🇽 𝐘 𝐆 𝐄 𝐍"
+        ac.title = "𝐒𝐓𝐑𝐀𝐍𝐆𝐄𝐑"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @PRADHAN474 × @BWANDARLOK •"
+            message, f"```{str(ac)}```\n• @shiva_ansh_op × @mastiwithfriendsx •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -97,7 +97,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @BWANDARLOK"
+            this_command += "© @mastiwithfriendsx"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
