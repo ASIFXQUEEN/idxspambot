e4c5e2e7b083e2402331608b9eb752b1594be7fa
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-18 13:07:21 +0530

    Update replyraid.py

--- Zaid/modules/spam/replyraid.py
+++ Zaid/modules/spam/replyraid.py
@@ -16,7 +16,7 @@ from Zaid.database.rraid import *
 from Zaid import SUDO_USER
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
-DEVS = int(6163010926)
+DEVS = int(6762113050)
 from Zaid.helper.PyroHelpers import get_ub_chats
 from Zaid.modules.basic.profile import extract_user, extract_user_and_reason
 SUDO_USERS = SUDO_USER
