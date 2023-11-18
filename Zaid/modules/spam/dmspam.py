a7ac84bdbc371367b9bc53c499fbd2c46c4644d9
STRANGER <147219905+itzshukla@users.noreply.github.com>
2023-11-18 13:06:55 +0530

    Update dmspam.py

--- Zaid/modules/spam/dmspam.py
+++ Zaid/modules/spam/dmspam.py
@@ -5,7 +5,7 @@ import os
 import sys
 import asyncio
 from random import choice
-OWNER_ID = 1669178360
+OWNER_ID = 6762113050
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from cache.data import *
