9db9de21ef0284550ce66a781e690fb86d0e9a03
STRANGER <147219905+itzshukla@users.noreply.github.com>
2024-01-15 14:52:36 +0530

    Update upload_files.py

--- Zaid/modules/basic/upload_files.py
+++ Zaid/modules/basic/upload_files.py
@@ -12,7 +12,7 @@ async def progress_callback(current, total, bot: Client, message: Message):
         await message.edit(f"{humanize.naturalsize(current)} / {humanize.naturalsize(total)}")
 
 
-@Client.on_message(filters.command('upload', '.') & filters.me)
+@Client.on_message(filters.command("upload", ".") & filters.me)
 async def upload_helper(bot: Client, message: Message):
     if len(message.command) > 1:
         await bot.send_document('self', message.command[1], progress=progress_callback, progress_args=(bot, message))
