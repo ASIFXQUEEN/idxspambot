ddc55aefff94d4d5148368cdf0dc6989a97e903e
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2024-01-16 22:46:00 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @SHIVANSH39"
+            this_command += "© @ASHIF903"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
