fc668865d09727c11b69f516a0de3c905bb59e49
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2024-01-16 22:14:23 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -44,7 +44,7 @@ async def module_help(client: Client, message: Message):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @SHIVANSH39 × @mastiwithfriendsx •",
+                f"```{str(ac)}```\n• @ASHIF903 × @BESTODISHA •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @SHIVANSH39 × @mastiwithfriendsx •"
+            message, f"```{str(ac)}```\n• @ASHIF903 × @BESTODISHA •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -97,7 +97,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @mastiwithfriendsx"
+            this_command += "© @ASHIF903"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
